38702113f9bf5885d0563b0fb79b8769e86cf673
alancotal <acotal2018@inf.udec.cl>
2022-04-25 22:01:11 -0400

    Actualización código base

--- app.py
+++ app.py
@@ -1,21 +1,35 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+def database():
+	'''
+	funcion para conectar con la base de datos
+
+
+	'''
+
+def edit_surveys():
+	"""
+		funcion para editar las encuestas
+	"""
+
+
+
 @app.route('/')
 def index():
 	return render_template("index.html")
+
+
+@app.route('/CreateSurvey')
+def create_survey():
+
+	return render_template("create_survey.html")#logica de crear encuesta
 '''
-@app.route('/crear_encuesta')
-def crear_encuesta():
-
-	return #logica de crear encuesta, agregar href en base.html
-
 @app.route('/encuestas')
-def ver_encuestas():
-	return #logica de ver encuestas
+def see_surveys():
 
 @app.route('responder_encuestas')
-def responder():
+def answer():
 	return #logica de responde encuestas
 
 '''
