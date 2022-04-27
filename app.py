82c79fe418816451dbd8c236f0f3e456cd017f68
dlinette <dcerda2018@inf.udec.cl>
2022-04-27 11:33:33 -0400

    Faltaba agregar la ruta y cambiar el nombre en create_survey

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ from db import Database
 app = Flask(__name__)
 app.config['MONGO_URI'] = "mongodb+srv://encuestas:encuestas@cluster0.m6eyi.mongodb.net/encuestas?retryWrites=true&w=majority"
 
-
 Database().setup(app)
 
 import routes.index  # nopep8
 import routes.create_survey  # nopep8
+import routes.apisurvey #nopep8
 
 
 if __name__ == '__main__':
