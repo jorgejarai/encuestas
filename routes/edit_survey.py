969289662b31e7603d49efebeab25463a9e9cb7c
rsanmarting <104087552+rsanmarting@users.noreply.github.com>
2022-04-29 12:56:24 -0400

    Visualizar preguntas para editar
    Se pueden ver las preguntas junto a sus alternativas dando la opción de editar

--- routes/edit_survey.py
+++ routes/edit_survey.py
@@ -1,22 +1,30 @@
 from __main__ import app
-from flask import flash, render_template, url_for, redirect,flash
+from flask import flash, render_template, url_for, redirect
 from db import surveys
 
 @app.route('/edit')
 def edit_survey():
     data = surveys.Surveys().get_all()
-    print(data)
     return render_template("edit_survey.html", encuestas = data)
 
 @app.route('/edit/<id>')
 def get_surveyid(id):
     data = surveys.Surveys().get_by_id(id)
-    print(data)
-    return render_template("edit_surveydata.html", encuesta = data)
+    questions = data.get('questions')
+    print(questions)
+    num_questions = len(questions)
+    num_alternatives = []
+    
+    for i in range(0,num_questions):
+        print(questions[i].get('alternatives'))
+        num_alternatives.insert(i, len(questions[i].get('alternatives')))
+    
+    print(num_alternatives)
+
+    return render_template("edit_surveydata.html", encuesta = data, num_q = num_questions, num_a = num_alternatives)
 
 @app.route('/delete/<string:id>')
 def delete_surveyid(id):
     print(id)
     surveys.Surveys().delete(id)
-    flash('Se ha eliminado correctamente')
     return redirect(url_for('edit_survey'))
