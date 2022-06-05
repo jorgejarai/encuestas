b597fb3cbd7b4d0c78c06f821ae1e30e894ea8fc
Jorge Jara <jorgejarainostroza@gmail.com>
2022-06-05 20:30:24 +0000

    save_answers: Normalizar payload de petición

--- routes/save_answers.py
+++ routes/save_answers.py
@@ -4,21 +4,22 @@ from db import surveys
 from bson.objectid import ObjectId
 from flask_cors import cross_origin
 
+
 @app.route("/surveys/<id>/answers/<user_id>", methods=['PUT'])
 @cross_origin()
 def save_answers(id, user_id):
-    answers = request.json["answers"]
-    answers["user_id"] = ObjectId(answers["user_id"])
+    user_id = ObjectId(request.json["user_id"])
+    answers = request.json["responses"]
 
-    for (i, resp) in enumerate(answers["responses"]):
+    for (i, resp) in enumerate(answers):
         if resp == "":
             return {
                 "status": "error",
                 "message": f"La pregunta {i + 1} no ha sido respondida"
-            }       
- 
+            }
+
     try:
-        surveys.Surveys().add_answers(id, answers)
+        surveys.Surveys().add_answers(user_id, answers)
     except ValueError as e:
         return {
             "status": "error",
