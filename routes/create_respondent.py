81c6f0d8cd450f708b1444576fa51f7f645ad504
Jorge Jara <jorgejarainostroza@gmail.com>
2022-06-06 18:51:54 +0000

    Forzar el agregado de sólo encuestados por CSV

--- routes/create_respondent.py
+++ routes/create_respondent.py
@@ -7,32 +7,30 @@ from io import TextIOWrapper
 import csv
 
 # Upload folder
-UPLOAD_FOLDER = 'static/files'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+UPLOAD_FOLDER = "static/files"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 # Get the upload files
 
 
-@app.route("/respondents/new", methods=['POST'])
+@app.route("/respondents/new", methods=["POST"])
 @cross_origin()
 def create_respondents():
     if not "application/json" in request.content_type:
-        csv_file = request.files['file']
-        if csv_file.filename != '':
-            csv_file = TextIOWrapper(csv_file, encoding='utf-8')
-            csv_reader = csv.reader(csv_file, delimiter=',')
+        csv_file = request.files["file"]
+        if csv_file.filename != "":
+            csv_file = TextIOWrapper(csv_file, encoding="utf-8")
+            csv_reader = csv.reader(csv_file, delimiter=",")
             next(csv_reader)
             usuarios = []
             for row in csv_reader:
                 nombre = row[0]
                 correo = row[1]
-                rol = row[2]
-                intereses = row[3]
+                intereses = row[2]
                 intereses = intereses.split(" ")
                 usuarios.append({
                     "Name": nombre,
                     "Email": correo,
-                    "Role": rol,
                     "Interests": intereses,
                 })
             import_users(usuarios)
@@ -42,11 +40,11 @@ def create_respondents():
                 "message": "Se esperaba un archivo"
             }
     else:
-        name = request.json['name']
-        email = request.json['email']
-        interests = request.json['interests']
+        name = request.json["name"]
+        email = request.json["email"]
+        interests = request.json["interests"]
 
-        Users().create(name=name, email=email, role='respondent', interests=interests)
+        Users().create(name=name, email=email, role="respondent", interests=interests)
 
     return {"status": "success"}
 
@@ -55,6 +53,5 @@ def create_respondents():
 
 def import_users(usuarios):
     for user in usuarios:
-        print(user['Interests'])
-        Users().create(name=user['Name'], email=user['Email'],
-                       role=user['Role'], interests=user['Interests'])
+        Users().create(name=user["Name"], email=user["Email"],
+                       role="respondent", interests=user["Interests"])
