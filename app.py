c37ee05182b01a8dee0db64c6844894765866e61
Jorge Jara <jorgejarainostroza@gmail.com>
2022-06-17 18:53:59 -0400

    Cambiar host de servidor a 0.0.0.0

--- app.py
+++ app.py
@@ -25,4 +25,4 @@ import routes.save_answers  # nopep8
 import routes.create_respondent  # nopep8
 
 if __name__ == '__main__':
-    app.run(port=5003, debug=True)
+    app.run(host="0.0.0.0", port=5003, debug=True)
