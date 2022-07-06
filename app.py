398a2ab7aa4800a11eb3429943969aba1dfbed98
Jorge Jara <jorgejarainostroza@gmail.com>
2022-07-06 18:43:40 -0400

    Configurar MongoDB en producción

--- app.py
+++ app.py
@@ -6,7 +6,11 @@ from db import Database
 import sys
 
 app = Flask(__name__)
-app.config['MONGO_URI'] = "mongodb+srv://encuestas:encuestas@cluster0.m6eyi.mongodb.net/encuestas?retryWrites=true&w=majority"
+if len(sys.argv) > 1 and sys.argv[1] == 'prod':
+    app.config['MONGO_URI'] = "mongodb+srv://encuestas:encuestas@cluster0.m6eyi.mongodb.net/encuestas?retryWrites=true&w=majority"
+else:
+    app.config['MONGO_URI'] = "mongodb://localhost:27017/encuestas"
+
 # mongodb+srv://encuestas:encuestas@cluster0.m6eyi.mongodb.net/encuestas?retryWrites=true&w=majority
 # mongodb://localhost:27017/encuestas
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
@@ -31,7 +35,7 @@ import routes.send_email  # nopep8
 import routes.get_statistics  # nopep8
 
 if __name__ == '__main__':
-    if sys.argv[1] == 'prod':
+    if len(sys.argv) > 1 and sys.argv[1] == 'prod':
         app.run(host="0.0.0.0", port=5003,
                 ssl_context=('cert.pem', 'key.pem'))
     else:
