8e93d43da8f2d238619ac05259f9ef195033c064
Jorge Jara <jorgejarainostroza@gmail.com>
2022-07-08 12:08:55 -0400

    Actualizar link de sesión

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ else:
 
 # mongodb+srv://encuestas:encuestas@cluster0.m6eyi.mongodb.net/encuestas?retryWrites=true&w=majority
 # mongodb://localhost:27017/encuestas
-cors = CORS(app, resources={r"/*": {"origins": "*"}})
-app.config['CORS_HEADERS'] = 'Content-Type'
+cors = CORS(app, resources={r"/*": {"origins": ["http://localhost:6003", "https://encuestas-frontend.vercel.app"]}})
+app.config['CORS_HEADERS'] = ['Content-Type', 'Authorization']
 
 app.config['AUTH0_DOMAIN'] = 'probable-fortnight.us.auth0.com'
 app.config['API_AUDIENCE'] = 'https://equipo3-is2-2022/api'
--- src/email_API.py
+++ src/email_API.py
@@ -11,35 +11,45 @@ from email.message import EmailMessage
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from db.link_sessions import LinkSessions
+import sys
+
+
 class Email:
 
     def __init__(self) -> None:
         self.server = SMTP(
-            host = getenv('SMTP_HOSTNAME'),
-            port = getenv('SMTP_TLS_PORT')
+            host=getenv('SMTP_HOSTNAME'),
+            port=getenv('SMTP_TLS_PORT')
         )
+
     def connect_server(self):
         self.server.starttls()
         self.server.login(
-            user = getenv('SMTP_USER'),
-            password = getenv('SMTP_PASSWORD')
+            user=getenv('SMTP_USER'),
+            password=getenv('SMTP_PASSWORD')
         )
 
-    def send_email(self,emails:list,subject: (str),**kwargs):
+    def send_email(self, emails: list, subject: (str), **kwargs):
         self.connect_server()
         aux = 0
         for email in emails:
-            aux +=1
+            aux += 1
             mime = MIMEMultipart()
             mime['From'] = getenv('SMTP_USER')
             mime['To'] = email
             mime['Subject'] = subject
-            link_path = LinkSessions().add(email,kwargs['id_encuesta'])
-            link = f"http://localhost:6003/{link_path}"
-            format = MIMEText(kwargs['message_format'].replace("$link_correo",link), kwargs['format'])
+            link_path = LinkSessions().add(email, kwargs['id_encuesta'])
+            link = None
+            if len(sys.argv) > 1 and sys.argv[1] == 'prod':
+                link = f"https://is2-2022.inf.udec.cl:5003/{link_path}"
+            else:
+                link = f"http://localhost:5003/{link_path}"
+            format = MIMEText(kwargs['message_format'].replace(
+                "$link_correo", link), kwargs['format'])
             mime.attach(format)
             try:
-                self.server.sendmail(getenv('SMTP_USER'),email,mime.as_string())
+                self.server.sendmail(getenv('SMTP_USER'),
+                                     email, mime.as_string())
             except Exception as e:
                 return {
                     "status": "error",
@@ -47,7 +57,6 @@ class Email:
                 }
             if(aux == len(emails)):
                 self.disconnect_server()
-
 
     def disconnect_server(self):
         self.server.quit()
