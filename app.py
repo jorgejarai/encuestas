8515edf49996829972df44080c0e021c9007fc96
Jorge Jara <jorgejarainostroza@gmail.com>
2022-07-08 12:17:10 -0400

    Redirigir / a frontend

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-from flask import Flask
-from flask_cors import CORS
+from flask import Flask, redirect
+from flask_cors import CORS, cross_origin
 from db import Database
 import sys
 
@@ -13,7 +13,8 @@ else:
 
 # mongodb+srv://encuestas:encuestas@cluster0.m6eyi.mongodb.net/encuestas?retryWrites=true&w=majority
 # mongodb://localhost:27017/encuestas
-cors = CORS(app, resources={r"/*": {"origins": ["http://localhost:6003", "https://encuestas-frontend.vercel.app"]}})
+cors = CORS(app, resources={
+            r"/*": {"origins": ["http://localhost:6003", "https://encuestas-frontend.vercel.app"]}})
 app.config['CORS_HEADERS'] = ['Content-Type', 'Authorization']
 
 app.config['AUTH0_DOMAIN'] = 'probable-fortnight.us.auth0.com'
@@ -22,7 +23,13 @@ app.config['ALGORITHMS'] = ['RS256']
 
 Database().setup(app)
 
-import routes  # nopep8
+
+@app.route('/')
+@cross_origin()
+def index():
+    return redirect('https://encuestas-frontend.vercel.app')
+
+
 import routes.create_survey  # nopep8
 import routes.edit_survey  # nopep8
 import routes.get_survey  # nopep8
